cc94b8f7efd70f3479224ed1a72ce75e01e7eb74
Nicole Asqui <nasqui@espol.edu.ec>
2024-09-27 09:06:25 -0500

    Add message valid

--- src/dto/Product.ts
+++ src/dto/Product.ts
@@ -2,24 +2,24 @@ import { IsDateString, IsNotEmpty, MaxLength, MinLength } from "class-validator"
 import { ProductInterface } from "../interfaces/product.interface";
 
 export class ProductDTO implements ProductInterface {
-    @IsNotEmpty()
+    @IsNotEmpty({ message: "The product ID is required." })
     id: string;
     
-    @MinLength(6)
-    @MaxLength(100)
+    @MinLength(6, { message: "The name must be at least 6 characters long." })
+    @MaxLength(100, { message: "The name can be up to 100 characters long." })
     name: string;
     
-    @MinLength(10)
-    @MaxLength(200)
+    @MinLength(10, { message: "The description must be at least 10 characters long." })
+    @MaxLength(200, { message: "The description can be up to 200 characters long." })
     description: string;
     
-    @IsNotEmpty()
+    @IsNotEmpty({ message: "The product logo is required." })
     logo: string;
     
-    @IsDateString()
+    @IsDateString({}, { message: "The release date must be a valid date string." })
     date_release: Date;
     
-    @IsDateString()
+    @IsDateString({}, { message: "The revision date must be a valid date string." })
     date_revision: Date;
 
     constructor(id: string, name: string, description: string, logo: string, date_release: Date, date_revision: Date) {
