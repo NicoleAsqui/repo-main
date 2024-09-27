5ebc7af4963c52c03b940c77d66cd4cb3ea98080
Nicole Asqui <nasqui@espol.edu.ec>
2024-09-27 09:02:20 -0500

    Fix product

--- src/const/message-error.const.ts
+++ src/const/message-error.const.ts
@@ -1,4 +1,6 @@
 export const MESSAGE_ERROR = {
-    'DuplicateIdentifier': "Duplicate identifier found in the database",
-    'NotFound': "Not product found with that identifier",
-}
+    'DuplicateIdentifier': "Duplicate identifier found in the database.",
+    'NotFound': "Product not found with that identifier.",
+    'ValidationError': "Validation error occurred.",
+    'InternalError': "An internal error occurred. Please try again later."
+};
--- src/dto/Product.ts
+++ src/dto/Product.ts
@@ -21,4 +21,13 @@ export class ProductDTO implements ProductInterface {
     
     @IsDateString()
     date_revision: Date;
+
+    constructor(id: string, name: string, description: string, logo: string, date_release: Date, date_revision: Date) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+        this.logo = logo;
+        this.date_release = date_release;
+        this.date_revision = date_revision;
+    }
 }
